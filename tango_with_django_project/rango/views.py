821f0fd6bebe392eb94950c8139428f79294fe6c
Abin Daniel <abindaniel@hotmail.co.uk>
2016-03-16 14:30:51 +0000

    completed chapter 10 with restricted page

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -215,9 +215,11 @@ def user_login(request):
         # blank dictionary object...
         return render(request, 'rango/login.html', {})
 
+
 @login_required
 def restricted(request):
-    return HttpResponse("Since you're logged in, you can see this text!")
+    return render(request, 'rango/restricted.html',{})
+
 
 # Use the login_required() decorator to ensure only those logged in can access the view.
 @login_required
